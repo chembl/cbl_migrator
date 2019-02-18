610a70838ffb14b6511349f3e73ae626feb6c58c
eloyfelix <eloyfelix@gmail.com>
2019-02-18 13:17:23 +0000

    update and cleanup
    * add logger
    
    * cleanup
    
    * change sysdate for current_timestamp

--- db_migrator/migrator.py
+++ db_migrator/migrator.py
@@ -1,11 +1,10 @@
-from sqlalchemy import UniqueConstraint, ForeignKeyConstraint, CheckConstraint, MetaData, PrimaryKeyConstraint, func, create_engine, inspect
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.base import ColumnCollection
 from sqlalchemy.util._collections import immutabledict
 from sqlalchemy.sql.elements import TextClause
 from sqlalchemy.schema import AddConstraint
 from sqlalchemy.sql import select
-from sqlalchemy.types import Numeric, Text, BigInteger, SmallInteger, Integer
+from sqlalchemy.types import Numeric, Text, BigInteger, SmallInteger, Integer, DateTime
 from sqlalchemy.dialects.oracle import NUMBER
 from sqlalchemy.dialects.mysql import (TINYINT as mysql_TINYINT,
                                        SMALLINT as mysql_SMALLINT,
@@ -13,14 +12,38 @@ from sqlalchemy.dialects.mysql import (TINYINT as mysql_TINYINT,
                                        INTEGER as mysql_INTEGER,
                                        BIGINT as mysql_BIGINT
                                        )
-import multiprocessing as mp
+from sqlalchemy import (UniqueConstraint,
+                        ForeignKeyConstraint,
+                        CheckConstraint,
+                        MetaData,
+                        PrimaryKeyConstraint,
+                        func,
+                        create_engine,
+                        inspect)
+from multiprocessing import cpu_count
+import concurrent.futures as cf
+import logging
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+logger = logging.getLogger('db_migrator')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler('db_migrator.log')
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 def fill_table(o_engine_conn, d_engine_conn, table_name, chunk_size):
     """
-    Filling tables with data.
+    Fills existing table in dest with origin table data.
     """
-    print("{} migrating".format(table_name))
+    logger.info('Migrating {} table'.format(table_name))
     o_engine = create_engine(o_engine_conn)
     o_metadata = MetaData()
     o_metadata.reflect(o_engine)
@@ -33,7 +56,7 @@ def fill_table(o_engine_conn, d_engine_conn, table_name, chunk_size):
     pk = pks[0]
     single_pk = True if len(pks) == 1 else False
 
-    # Check if the table exists in migrated db, if needs to be completed and mark starting id
+    # Check if the table exists in migrated db, if needs to be completed and set starting pk id
     try:
         first_it = True
         d_table = d_metadata.tables[table_name]
@@ -42,7 +65,8 @@ def fill_table(o_engine_conn, d_engine_conn, table_name, chunk_size):
         d_count = d_engine.execute(select([func.count(dpk)])).fetchone()[0]
         # Nothing to do here
         if count == d_count:
-            print(table_name, "is already migrated")
+            logger.info(
+                '{} table exists in dest and has same counts than same table in origin. Skipping.')
             return True
         elif count != d_count and d_count != 0:
             q = select([d_table]).order_by(dpk.desc()).limit(1)
@@ -50,8 +74,12 @@ def fill_table(o_engine_conn, d_engine_conn, table_name, chunk_size):
             next_id = res.fetchone().__getitem__(dpk.name)
             first_it = False
     except:
-        print("need to create {} table before filling it".format(table_name))
+        logger.info(
+            'Need to create {} table before filling it'.format(table_name))
 
+    # table has a composite pk (usualy a bad design choice). 
+    # Uses offset and limit to copy data from origin to dest, 
+    # not v good performance.
     if not single_pk:
         if not first_it:
             offset = d_count
@@ -65,11 +93,13 @@ def fill_table(o_engine_conn, d_engine_conn, table_name, chunk_size):
             d_engine.execute(
                 table.insert(),
                 [
-                    dict([(col_name, col_value) for col_name, col_value in zip(res.keys(), row)])
+                    dict([(col_name, col_value)
+                          for col_name, col_value in zip(res.keys(), row)])
                     for row in data
                 ]
             )
     else:
+        # table has a single pk field. Sorting by pk and paginating.
         while True:
             q = select([table]).order_by(pk).limit(chunk_size)
             if not first_it:
@@ -83,32 +113,46 @@ def fill_table(o_engine_conn, d_engine_conn, table_name, chunk_size):
                 d_engine.execute(
                     table.insert(),
                     [
-                        dict([(col_name, col_value) for col_name, col_value in zip(res.keys(), row)])
+                        dict([(col_name, col_value)
+                              for col_name, col_value in zip(res.keys(), row)])
                         for row in data
                     ]
                 )
             else:
                 break
-    print(table_name, "migrated")
+    logger.info('{} table filled'.format(table_name))
     return True
 
 
 class DbMigrator(object):
+    """Migrator class.
 
+    Migrates origin db to dest one.
+
+    Attributes:
+        o_conn_string: Origin DB connection string.
+        d_conn_string: Dest DB connection string.
+        n_workers: Number of processes.
+
+    Methods
+        copy_schema: Runs a on memory similarity search
+        copy_constraints: Runs a on disk similarity search
+        copy_indexes: Runs a on memory substructure screenout
+        migrate: Runs a on disk substructure screenout
+    """
     o_engine_conn = None
     d_engine_conn = None
     n_cores = None
 
-    def __init__(self, o_conn_string, d_conn_string, n_cores=mp.cpu_count()):
+    def __init__(self, o_conn_string, d_conn_string, n_workers=cpu_count()):
         self.o_engine_conn = o_conn_string
         self.d_engine_conn = d_conn_string
-        self.n_cores = n_cores
+        self.n_cores = n_workers
 
-
-    def fix_column_type(self, col, db_engine):
+    def __fix_column_type(self, col, db_engine):
         """
         Adapt column types to the most reasonable generic types (ie. VARCHAR -> String)
-        Based on sqlacodegen.
+        Inspired on sqlacodegen.
         """
         cls = col.type.__class__
         for supercls in cls.__mro__:
@@ -117,6 +161,13 @@ class DbMigrator(object):
             if supercls.__name__ != supercls.__name__.upper() and not supercls.__name__.startswith('_'):
                 break
         col.type = col.type.adapt(cls)
+
+        # replace oracle sysdate with current_timestamp, compatible with postgresql
+        if isinstance(col.type, DateTime):
+            if hasattr(col.server_default, 'arg'):
+                if col.server_default.arg.text.strip() == 'sysdate':
+                    col.server_default.arg.text = 'current_timestamp'
+
         if isinstance(col.type, Numeric):
             if col.type.scale == 0:
                 if db_engine == 'mysql':
@@ -162,10 +213,10 @@ class DbMigrator(object):
                 col.type.length = 100000
         return col
 
-
-    def migrate_tables(self):
-        """
-        Copy schema to anther db, as we are migrating from release schema we copy everything.
+    def copy_schema(self):
+        """        
+        Copies the schema to dest db.
+        Copies all constraints in sqlite, only pk in mysql and postgres.
         """
         o_engine = create_engine(self.o_engine_conn)
         d_engine = create_engine(self.d_engine_conn)
@@ -175,14 +226,19 @@ class DbMigrator(object):
 
         new_metadata_tables = {}
         for table_name, table in metadata.tables.items():
-            # Keep everything for sqlite. SQLite cant alter table ADD CONSTRAINT. Only 1 simultaneous process can write to it.
-            # Keep only PKs for PostreSQL and MySQL. Restoring them after all data is copied.
-            keep_constraints = list(filter(lambda cons: isinstance(cons, PrimaryKeyConstraint), table.constraints))
+            # Keep everything for sqlite. SQLite cant alter table ADD CONSTRAINT. 
+            # Only 1 simultaneous process can write to it.
+            # Keep only PKs for PostreSQL and MySQL. 
+            # Restoring them after all data is copied.
+            keep_constraints = list(filter(lambda cons: isinstance(
+                cons, PrimaryKeyConstraint), table.constraints))
             if d_engine.name == 'sqlite':
                 uks = insp.get_unique_constraints(table_name)
                 for uk in uks:
-                    uk_cols = filter(lambda c: c.name in uk['column_names'], table._columns)
-                    keep_constraints.append(UniqueConstraint(*uk_cols, name=uk['name']))
+                    uk_cols = filter(
+                        lambda c: c.name in uk['column_names'], table._columns)
+                    keep_constraints.append(
+                        UniqueConstraint(*uk_cols, name=uk['name']))
                 for fk in filter(lambda cons: isinstance(cons, ForeignKeyConstraint), table.constraints):
                     keep_constraints.append(fk)
                 for cc in filter(lambda cons: isinstance(cons, CheckConstraint), table.constraints):
@@ -198,7 +254,7 @@ class DbMigrator(object):
             new_metadata_cols = ColumnCollection()
             for col in table._columns:
                 col.comment = None
-                col = self.fix_column_type(col, d_engine.name)
+                col = self.__fix_column_type(col, d_engine.name)
                 # be sure that no column has auto-increment
                 col.autoincrement = False
                 new_metadata_cols.add(col)
@@ -208,8 +264,10 @@ class DbMigrator(object):
         metadata.tables = immutabledict(new_metadata_tables)
         metadata.create_all(d_engine)
 
-
     def validate_migration(self):
+        """
+        Checks that counts for all tables in origin an dest dbd are equal.
+        """
         o_engine = create_engine(self.o_engine_conn)
         o_metadata = MetaData()
         o_metadata.reflect(o_engine)
@@ -226,17 +284,20 @@ class DbMigrator(object):
         for table_name, table in o_metadata.tables.items():
             migrated_table = d_metadata.tables[table_name]
             if o_s.query(table).count() != d_s.query(migrated_table).count():
-                print('Row count failed for table {}, {}, {}'.format(table_name,
-                                                                     o_s.query(table).count(),
-                                                                     d_s.query(migrated_table).count()))
+                logger.info('Row count failed for table {}, {}, {}'.format(table_name,
+                                                                           o_s.query(
+                                                                               table).count(),
+                                                                           d_s.query(migrated_table).count()))
+
                 validated = False
         o_s.close()
         d_s.close()
         return validated
 
-
-    def migrate_constraints(self):
-
+    def copy_constraints(self):
+        """
+        Migrates constraints, UKs, CCs and FKs.
+        """
         o_engine = create_engine(self.o_engine_conn)
         d_engine = create_engine(self.d_engine_conn)
         metadata = MetaData()
@@ -245,35 +306,38 @@ class DbMigrator(object):
         insp = inspect(o_engine)
 
         for table_name, table in metadata.tables.items():
-            keep_constraints = []
-
+            constraints_to_keep = []
             # keep unique constraints
             uks = insp.get_unique_constraints(table_name)
             for uk in uks:
-                uk_cols = filter(lambda c: c.name in uk['column_names'], table._columns)
+                uk_cols = filter(
+                    lambda c: c.name in uk['column_names'], table._columns)
                 uuk = UniqueConstraint(*uk_cols, name=uk['name'])
                 uuk._set_parent(table)
-                keep_constraints.append(uuk)
+                constraints_to_keep.append(uuk)
 
             # keep check constraints
-            ccs = filter(lambda cons: isinstance(cons, CheckConstraint), table.constraints)
+            ccs = filter(lambda cons: isinstance(
+                cons, CheckConstraint), table.constraints)
             for cc in ccs:
                 cc.sqltext = TextClause(str(cc.sqltext).replace("\"", ""))
-                keep_constraints.append(cc)
+                constraints_to_keep.append(cc)
 
             # keep fks
             for fk in filter(lambda cons: isinstance(cons, ForeignKeyConstraint), table.constraints):
-                keep_constraints.append(fk)
+                constraints_to_keep.append(fk)
 
             # create all constraints
-            for cons in keep_constraints:
+            for cons in constraints_to_keep:
                 try:
                     d_engine.execute(AddConstraint(cons))
                 except Exception as e:
-                    print(e)
+                    logger.info(e)
 
-
-    def migrate_indexes(self):
+    def copy_indexes(self):
+        """
+        Copies the indexes when it is possible.
+        """
         o_engine = create_engine(self.o_engine_conn)
         d_engine = create_engine(self.d_engine_conn)
         metadata = MetaData()
@@ -283,19 +347,22 @@ class DbMigrator(object):
 
         for table_name, table in metadata.tables.items():
             uks = insp.get_unique_constraints(table_name)
-            indexes_to_keep = filter(lambda index: index.name not in [x['name'] for x in uks], table.indexes)
+            # UKs are internally implemented as a unique indexes. 
+            # Don't create index if it exists a UK for that field.
+            indexes_to_keep = filter(lambda index: index.name not in [
+                                     x['name'] for x in uks], table.indexes)
 
             for index in indexes_to_keep:
                 try:
                     index.create(d_engine)
                 except Exception as e:
-                    print(e)
+                    logger.info(e)
 
-
-    def sort_tables(self):
+    def __sort_tables(self):
         """
         Sort tables by FK dependency. 
-        SQLite cannot ALTER to add constraints and only supports one write process simultaneously.
+        SQLite cannot ALTER to add constraints 
+        and only supports one concurrent write process.
         """
         o_engine = create_engine(self.o_engine_conn)
         metadata = MetaData()
@@ -319,22 +386,35 @@ class DbMigrator(object):
                 tables.append(tables.pop(0))
         return ordered_tables
 
-
     def migrate(self, copy_schema=True, copy_data=True, copy_constraints=True, copy_indexes=True, chunk_size=1000):
-
+        """
+        Copies origin database to dest.
+        """
         d_engine = create_engine(self.d_engine_conn)
 
-        # copy schema
+        # copy tables from origin to dest
         if copy_schema:
-            self.migrate_tables()
+            self.copy_schema()
 
-        # copy data
+        # fill tables in dest
         if copy_data:
-            tables = self.sort_tables()
+            tables = self.__sort_tables()
             # SQLite accepts concurrent read but not write
             processes = 1 if d_engine.name == 'sqlite' else self.n_cores
-            with mp.Pool(processes=processes) as pool:
-                pool.starmap(fill_table, [(self.o_engine_conn, self.d_engine_conn, x, chunk_size) for x in tables])
+
+            with cf.ProcessPoolExecutor(max_workers=processes) as exe:
+                futures = {exe.submit(fill_table, self.o_engine_conn, self.d_engine_conn, table, chunk_size):
+                           table for table in tables}
+
+            for future in cf.as_completed(futures):
+                table = futures[future]
+                try:
+                    res = future.result()
+                    if not res:
+                        logger.info(
+                            'Something went wrong when copying table {}: '.format(table))
+                except Exception as e:
+                    logger.info('Table {} worker died: '.format(table), e)
 
         # check row counts for each table
         if not copy_data:
@@ -342,11 +422,13 @@ class DbMigrator(object):
         else:
             all_migrated = self.validate_migration()
 
-        # create constraints
+        # copy constraints and indexes
         if all_migrated:
+            # do not migrate constraints in sqlite, we initially kept all of them as 
+            # it does not support alter table ADD CONSTRAINT.
             if copy_constraints and d_engine.name != 'sqlite':
-                self.migrate_constraints()
+                self.copy_constraints()
             if copy_indexes:
-                self.migrate_indexes()
+                self.copy_indexes()
 
         return all_migrated
--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup
 
-
 if __name__ == '__main__':
     setup(
         name='db_migrator',
-        version='0.1',
-        author='Eloy Felix',
+        version='0.0.2',
+        author='Eloy Félix',
         author_email='',
-        description='Migrate Oracle dbs to PostgreSQL, MySQL and Sqlite',
+        description='Migrates Oracle dbs to PostgreSQL, MySQL and Sqlite',
         url='',
         license='MIT',
         packages=['db_migrator', ],
-        long_description=open('README.md').read(),
+        long_description=open('README.md', encoding='utf-8').read(),
         install_requires=[
             'SQLAlchemy>=1.2',
         ],
         include_package_data=True,
         classifiers=['Development Status :: 2 - Pre-Alpha',
                      'Intended Audience :: Developers',
-                     'License :: Creative Commons :: Attribution-ShareAlike 3.0 Unported',
+                     'License :: OSI Approved :: MIT License',
                      'Operating System :: POSIX :: Linux',
-                     'Programming Language :: Python :: 3.5',
-                     'Programming Language :: Python :: 3.6'],
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7'],
         zip_safe=True,
     )
