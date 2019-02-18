ee1ce01ecc8fa72d08bba98bca928642400b48a6
Eloy Felix <eloyfelix@gmail.com>
2019-02-18 10:58:04 +0000

    clean

--- db_migrator/migrator.py
+++ db_migrator/migrator.py
@@ -29,14 +29,11 @@ import logging
 
 logger = logging.getLogger('db_migrator')
 logger.setLevel(logging.DEBUG)
-# create a file handler
 handler = logging.FileHandler('db_migrator.log')
 handler.setLevel(logging.DEBUG)
-# create a logging format
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
-# add the handlers to the logger
 logger.addHandler(handler)
 
 # ----------------------------------------------------------------------------------------------------------------------
