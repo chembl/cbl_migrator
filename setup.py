df871e760d8cd94255507c772e6909f4d41e3f2a
Eloy Felix <eloyfelix@gmail.com>
2020-01-30 19:37:35 +0000

    update license

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name='cbl_migrator',
-        version='0.1.4',
+        version='0.1.5',
         author='Eloy Félix',
         author_email='eloyfelix@gmail.com',
         description='Migrates Oracle dbs to PostgreSQL, MySQL and Sqlite',
