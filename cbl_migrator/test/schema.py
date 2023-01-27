252074826687dea7559cc0d43781164c08a61db6
Eloy Felix <eloyfelix@gmail.com>
2023-01-27 10:36:00 +0000

    sqla2.0

--- cbl_migrator/test/schema.py
+++ cbl_migrator/test/schema.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, ForeignKey, CheckConstraint
 from sqlalchemy.types import String, Integer, Float, Text
 
